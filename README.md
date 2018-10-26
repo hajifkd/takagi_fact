10388186471771b668947bdb4f2aab5f71cf5a38
Hajime Fukuda <hajime.fukuda@me.com>
2018-10-26 11:01:10 +0900

    added files for PyPI

---
+++ README.md
@@ -0,0 +1,7 @@
+# takagi\_fact
+
+A tiny python library for the symmetric singular value decomposition and the Takagi factorization
+
+## Usage
+
+See [here](https://github.com/hajifkd/takagi_fact/blob/master/tests/test.py).
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="takagi_fact",
+    version="0.0.1",
+    author="Hajime Fukuda",
+    author_email="hajime.fukuda@me.com",
+    description="A libary for the symmetric SVD and the Takagi factorization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hajifkd/takagi_fact/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+)
